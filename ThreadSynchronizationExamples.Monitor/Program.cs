08e225f2538e20b7c4f9355b5e3398a246c54da0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-22 19:38:07 +0400

    Monitor usage

---
+++ ThreadSynchronizationExamples.Monitor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronizationExamples.MonitorUsage
+{
+    class Program
+    {
+        private static readonly object Locker = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(DoWork).Start();
+            }
+        }
+
+        public static void DoWork()
+        {
+            try
+            {
+                Monitor.Enter(Locker);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starting...");
+                Thread.Sleep(2000);
+                throw new CustomThreadException();
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} completed!");
+            }
+            catch (CustomThreadException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Monitor.Exit(Locker);
+            }
+        }
+    }
+
+    public class CustomThreadException : Exception
+    {
+        public CustomThreadException()
+            :base("Error occured while completing thread.")
+        {
+            
+        }
+    }
+}
