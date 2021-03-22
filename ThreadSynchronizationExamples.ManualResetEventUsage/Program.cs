5a5663006a55732d3a762fa292736521a981b553
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-22 19:37:55 +0400

    Manual Reset Event usage

---
+++ ThreadSynchronizationExamples.ManualResetEventUsage/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronizationExamples.ManualResetEventUsage
+{
+    class Program
+    {
+        private static readonly ManualResetEvent Mre = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            new Thread(Write).Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Read).Start();
+            }
+        }
+
+        public static void Write()
+        {
+            ThreadLogger("writing...");
+            Mre.Reset();
+            Thread.Sleep(5000);
+            ThreadLogger("completed!");
+            Mre.Set();
+        }
+
+        public static void Read()
+        {
+            ThreadLogger("waiting to read...");
+            Mre.WaitOne();
+            ThreadLogger("reading...");
+            Thread.Sleep(1000);
+            ThreadLogger("completed!");
+        }
+
+        private static void ThreadLogger(string msg)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is {msg}");
+        }
+    }
+}
