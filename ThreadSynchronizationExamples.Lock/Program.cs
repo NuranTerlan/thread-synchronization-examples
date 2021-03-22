8b0492ed47e85176e4d823edceabc4db5861df4d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-22 19:37:42 +0400

    Lock usage

---
+++ ThreadSynchronizationExamples.Lock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronizationExamples.LockUsage
+{
+    class Program
+    {
+        private static readonly object Locker = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(DoWork).Start();
+            }
+        }
+
+        public static void DoWork()
+        {
+            lock (Locker)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starting...");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} completed!");
+            }
+        }
+    }
+}
