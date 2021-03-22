9d94ba10eabf2173ba6c8b57ab8be18f85367d64
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-22 19:37:33 +0400

    Auto Reset Event and Mutex usages

---
+++ ThreadSynchronizationExamples.AutoResetEvent-Mutex/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronizationExamples.AutoResetEvent_MutexUsage
+{
+    class Program
+    {
+        //private static readonly AutoResetEvent Are = new AutoResetEvent(true);
+        private static readonly Mutex Mutex = new Mutex();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Write).Start();
+            }
+
+            //int count = 0;
+            //foreach (var result in GetCalculations(100))
+            //{
+            //    count++;
+            //    Console.WriteLine($"Result of {count} --> {result}");
+            //}
+
+            //Thread.Sleep(3000);
+            //Are.Set();
+        }
+
+        //public static IEnumerable<int> GetCalculations(int size)
+        //{
+        //    var random = new Random();
+
+        //    var a = new List<int>(size);
+        //    var b = new List<int>(size);
+
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        a.Add(random.Next(100, 1000));
+        //        b.Add(random.Next(100, 1000));
+        //    }
+
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        Thread.Sleep(50);
+        //        yield return a[i] * b[i];
+        //    }
+        //}
+
+        public static void Write()
+        {
+            ThreadLogger("waiting...");
+            Mutex.WaitOne();
+            ThreadLogger("writing...");
+            Thread.Sleep(1000);
+            ThreadLogger("completed!");
+            Mutex.ReleaseMutex();
+        }
+
+        private static void ThreadLogger(string msg)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is {msg}");
+        }
+    }
+}
