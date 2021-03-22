756a29d6e8bfe912eb2648e8e2db94257cf224d9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-22 19:38:18 +0400

    Semaphore usage

---
+++ ThreadSynchronizationExamples.SemaphoreUsage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronizationExamples.SemaphoreUsage
+{
+    class Program
+    {
+        private static readonly Semaphore Semaphore = new Semaphore(2, 2);
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Write).Start();
+            }
+        }
+
+        public static void Write()
+        {
+            ThreadLogger("waiting...");
+            Semaphore.WaitOne();
+            ThreadLogger("writing...");
+            Thread.Sleep(3000);
+            ThreadLogger("completed!");
+            Semaphore.Release();
+        }
+
+        private static void ThreadLogger(string msg)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} is {msg}");
+        }
+    }
+}
